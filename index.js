76b2fb2311c41842a659c7c58d2b860757f5cec9
IssaDev99 <zawarudo74u@gmail.com>
2024-09-24 14:39:23 +0200

    commit1

---
+++ index.js
@@ -0,0 +1,32 @@
+const form = document.querySelector("form");
+
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquer sur un todo pour le supprimer</li>`;
+  }
+}
+
+window.addEventListener("load", getTodos);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+  storeList();
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
